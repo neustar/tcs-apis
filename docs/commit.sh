a403175d5e1e58ca3c98bd51d768f7f6e3b34215
Rishabh Srivastava <rishabh.srivastava@team.neustar>
2021-08-30 12:20:49 -0400

    updated paths

--- docs/commit.sh
+++ docs/commit.sh
@@ -5,4 +5,4 @@ git push
 git rev-parse HEAD
 COMMIT_ID=$(git rev-parse --verify HEAD)
 echo ${COMMIT_ID} is the last hash
-docker run -p 8087:80 -e SPEC_URL=https://raw.githubusercontent.com/rishagit/cid-oas/${COMMIT_ID}/docs/neustar-cid.yaml -e PAGE_TITLE="Neustar CID" -e PAGE_FAVICON="https://raw.githubusercontent.com/rishagit/cid-oas/main/docs/neustar-favicon.png" redocly/redoc
+docker run -p 8087:80 -e SPEC_URL=https://raw.githubusercontent.com/neustar/tcs-apis/${COMMIT_ID}/docs/neustar-cid.yaml -e PAGE_TITLE="Neustar CID" -e PAGE_FAVICON="https://raw.githubusercontent.com/neustar/tcs-apis/main/docs/neustar-favicon.png" redocly/redoc
